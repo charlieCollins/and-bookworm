6d24a5cff692a20ee08d9115362b07787a325123
simonmclaughlin.co.uk <simonmclaughlin.co.uk@bf2e8616-0238-11df-af12-d347ead69036>
2010-08-30 00:05:17 +0000

    Updated with new Open Library search method

--- src/com/totsp/bookworm/data/OpenLibraryDataSource.java
+++ src/com/totsp/bookworm/data/OpenLibraryDataSource.java
@@ -1,14 +1,5 @@
 package com.totsp.bookworm.data;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Date;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.util.Log;
 
 import com.totsp.bookworm.BookWormApplication;
@@ -17,247 +8,218 @@ import com.totsp.bookworm.model.Author;
 import com.totsp.bookworm.model.Book;
 import com.totsp.bookworm.util.DateUtil;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Data source for openlibrary.org
  * 
  * @author Simon McLaughlin
  */
 public class OpenLibraryDataSource implements BookDataSource {
-	
-	private static final String OL_ISBN_SEARCH_PREFIX = "http://openlibrary.org/api/books?callback=processOLBooks&details=true&bibkeys=ISBN:";
-	private static final String OL_BOOK_SEARCH_PREFIX = "http://openlibrary.org/api/search?q=";
-	private static final String OL_BOOK_DATA_PREFIX = "http://openlibrary.org/api/get?key=";
-	
-	private JSONObject jObject;
-	private final HttpHelper httpHelper;
-	
-	private boolean debugEnabled;
-	
-	public OpenLibraryDataSource() {
-		httpHelper = new HttpHelper();
-	}
-	
-	public void setDebugEnabled(final boolean debugEnabled) {
-	      this.debugEnabled = debugEnabled;
-	}
-	
-	public Book getBook(String isbn) {
-		String url = OL_ISBN_SEARCH_PREFIX+isbn;
-		String response = httpHelper.performGet(url, null, null, null);
-		if (debugEnabled) {
-			Log.d(Constants.LOG_TAG, "HTTP request to URL " + url);
-			Log.d(Constants.LOG_TAG, "HTTP response\n" + response);
-		}
-		if ((response == null) || response.contains(HttpHelper.HTTP_RESPONSE_ERROR)) {
-			Log.w(Constants.LOG_TAG, "HTTP request returned no data (null) - " + url);
-			return null;
-		}
-		
-		ArrayList<Book> books = new ArrayList<Book>();
-		try {
-			books = parseResponse(response, isbn);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if ((books != null) && !books.isEmpty()) {
-			return books.get(0);
-		}
-		return null;
-	}
 
-	private ArrayList<Book> parseResponse(String response, String isbn) throws JSONException {
-		ArrayList<Book> books = new ArrayList<Book>();
-		
-		int stop = response.length()-2;
-		String jsonString = response.substring(15, stop);
-		
-		if ((jsonString.length() == 2)) {
-			Log.w(Constants.LOG_TAG, "HTTP request returned no data (null) - " + OL_ISBN_SEARCH_PREFIX+isbn);
-			return null;
-		}
-		
-		jObject = new JSONObject(jsonString);
-		Book book = new Book();
-		String key = "ISBN:"+isbn;
-				
-		JSONObject jsonBook = jObject.getJSONObject(key);
-		
-		book.title = (jsonBook.getJSONObject("details").getString("title"));
-		
-		if(jsonBook.getJSONObject("details").has("isbn_10")) {
-			book.isbn10 = (jsonBook.getJSONObject("details").getJSONArray("isbn_10").getString(0));
-		}
-		
-		if(jsonBook.getJSONObject("details").has("isbn_13")) {
-			book.isbn13 = (jsonBook.getJSONObject("details").getJSONArray("isbn_13").getString(0));
-		}
-		
-		if(jsonBook.getJSONObject("details").has("subtitle")) {
-			book.subTitle = (jsonBook.getJSONObject("details").getString("subtitle"));
-		}
-		
-		if(jsonBook.getJSONObject("details").has("publish_date")) {
-			Date d = DateUtil.parse(jsonBook.getJSONObject("details").getString("publish_date"));
-			if (d != null) {
-				book.datePubStamp = (d.getTime());
-			}
-		}
-		
-		if(jsonBook.getJSONObject("details").has("authors")) {
-			book.authors.add(new Author(jsonBook.getJSONObject("details").getJSONArray("authors").getJSONObject(0).getString("name")));
-		}
-		
-		if(jsonBook.getJSONObject("details").has("publishers")) {
-			book.publisher = (jsonBook.getJSONObject("details").getJSONArray("publishers").getString(0));
-		}
-		
-		if(jsonBook.getJSONObject("details").has("subjects")) {
-			JSONArray resultArray = jsonBook.getJSONObject("details").getJSONArray("subjects");
-			String subjects = "";
-			
-			for (int i = 0; i < resultArray.length(); i++) {
-				subjects += resultArray.getString(i);
-			}
-			book.subject = subjects;
-		}		
-		
-		if(jsonBook.getJSONObject("details").has("physical_format")) {
-			book.format = (jsonBook.getJSONObject("details").getString("physical_format"));
-		}
-		
-		books.add(book);
-		if ((books != null) && !books.isEmpty()) {
-			return books;
-		}
-		return null;
-	}
+   private static final String OL_ISBN_SEARCH_PREFIX =
+            "http://openlibrary.org/api/books?callback=processOLBooks&details=true&bibkeys=ISBN:";
+   private static final String OL_BOOK_SEARCH_PREFIX = "http://openlibrary.org/search.json?title=";
+   private static final String OL_BOOK_DATA_PREFIX = "http://openlibrary.org/api/get?key=";
 
-	public ArrayList<Book> getBooks(String searchTerm, int startIndex) {
-		if(startIndex == 0)
-		{
-			String url = OL_BOOK_SEARCH_PREFIX;
-			try {
-				url = url+URLEncoder.encode("{\"query\":\""+searchTerm+"\"}","UTF-8");
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			url = url.replaceAll("%2B", "%20");
-			
-			String response = httpHelper.performGet(url, null, null, null);
-			
-			if (debugEnabled) {
-				Log.d(Constants.LOG_TAG, "HTTP request to URL " + url);
-				Log.d(Constants.LOG_TAG, "HTTP response\n" + response);
-			}
-			
-			if ((response == null) || response.contains(HttpHelper.HTTP_RESPONSE_ERROR)) {
-				Log.w(Constants.LOG_TAG, "HTTP request returned no data (null) - " + url);
-				return null;
-			}
-			
-			ArrayList<Book> books = null;
-			try {
-				books = getBookDetails(response);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			if ((books != null) && !books.isEmpty()) {
-				return books;
-			}
-		}
-		return null;
+   private final BookWormApplication application;
+   private final HttpHelper httpHelper;
 
-	}
+   private JSONObject jObject;
 
-	private ArrayList<Book> getBookDetails(String jsonString) throws JSONException {
-		ArrayList<Book> books = new ArrayList<Book>(); 
-		jObject = new JSONObject(jsonString);
-		JSONArray resultArray = jObject.getJSONArray("result");
-				
-		for (int i = 0; i < resultArray.length(); i++) {
-			String url = OL_BOOK_DATA_PREFIX+resultArray.getString(i);
-			String response = httpHelper.performGet(url, null, null, null);
-			
-			if (debugEnabled) {
-				Log.d(Constants.LOG_TAG, "HTTP request to URL " + url);
-				Log.d(Constants.LOG_TAG, "HTTP response\n" + response);
-			}
-			
-			JSONObject jsonBook = new JSONObject(response);
-			Book book = new Book();
-			String title = "";
-			if(jsonBook.getJSONObject("result").has("title_prefix")) {
-				title += jsonBook.getJSONObject("result").getString("title_prefix");
-			}
-			title += jsonBook.getJSONObject("result").getString("title");
-			book.title = (title);
-			
-			if(jsonBook.getJSONObject("result").has("isbn_10")) {
-				book.isbn10 = (jsonBook.getJSONObject("result").getJSONArray("isbn_10").getString(0));
-			}
-			
-			if(jsonBook.getJSONObject("result").has("isbn_13")) {
-				book.isbn13 = (jsonBook.getJSONObject("result").getJSONArray("isbn_13").getString(0));
-			}
-			
-			if(jsonBook.getJSONObject("result").has("subtitle")) {
-				book.subTitle = (jsonBook.getJSONObject("result").getString("subtitle"));
-			}
-			
-			if(jsonBook.getJSONObject("result").has("publish_date")) {
-				Date d = DateUtil.parse(jsonBook.getJSONObject("result").getString("publish_date"));
-				if (d != null) {
-					book.datePubStamp = (d.getTime());
-				}
-			}
-			
-			if(jsonBook.getJSONObject("result").has("authors")) {
-				String url2 = OL_BOOK_DATA_PREFIX+jsonBook.getJSONObject("result").getJSONArray("authors").getJSONObject(0).getString("key");
-				String response2 = httpHelper.performGet(url2, null, null, null);
-				
-				if (debugEnabled) {
-					Log.d(Constants.LOG_TAG, "HTTP request to URL " + url2);
-					Log.d(Constants.LOG_TAG, "HTTP response\n" + response2);
-				}
-				
-				JSONObject jsonAuthor = new JSONObject(response2);
-				book.authors.add(new Author(jsonAuthor.getJSONObject("result").getString("name")));
-			}
-			
-			if(jsonBook.getJSONObject("result").has("publishers")) {
-				book.publisher = (jsonBook.getJSONObject("result").getJSONArray("publishers").getString(0));
-			}
-			
-			if(jsonBook.getJSONObject("result").has("subjects")) {
-				JSONArray resultArray2 = jsonBook.getJSONObject("result").getJSONArray("subjects");
-				String subjects = "";
-				
-				for (int j = 0; j < resultArray2.length(); j++) {
-					subjects += resultArray2.getString(j);
-				}
-				book.subject = subjects;
-			}		
-			
-			if(jsonBook.getJSONObject("result").has("physical_format")) {
-				book.format = (jsonBook.getJSONObject("result").getString("physical_format"));
-			}
-			
-			books.add(book);
-		}
-		
-		if ((books != null) && !books.isEmpty()) {
-			return books;
-		}
-		return null;
-	}
+   public OpenLibraryDataSource(final BookWormApplication application) {
+      this.application = application;
+      this.httpHelper = new HttpHelper();
+   }
 
-	public void setContext(BookWormApplication appContext) {
-		// TODO Auto-generated method stub
-		
-	}
+   public Book getBook(String isbn) {
+      String url = OpenLibraryDataSource.OL_ISBN_SEARCH_PREFIX + isbn;
+      String response = httpHelper.performGet(url, null, null, null);
+      if (application.debugEnabled) {
+         Log.d(Constants.LOG_TAG, "HTTP request to URL " + url);
+         Log.d(Constants.LOG_TAG, "HTTP response\n" + response);
+      }
+      if ((response == null) || response.contains(HttpHelper.HTTP_RESPONSE_ERROR)) {
+         Log.w(Constants.LOG_TAG, "HTTP request returned no data (null) - " + url);
+         return null;
+      }
 
+      Book book = null;
+      try {
+         book = parseResponse(response, isbn);
+      } catch (JSONException e) {
+         Log.e(Constants.LOG_TAG, e.getMessage(), e);
+      }
+      return book;
+   }
+
+   private Book parseResponse(String response, String isbn) throws JSONException {
+
+      int stop = response.length() - 2;
+      String jsonString = response.substring(15, stop);
+
+      if ((jsonString.length() == 2)) {
+         Log.w(Constants.LOG_TAG, "HTTP request returned no data (null) - "
+                  + OpenLibraryDataSource.OL_ISBN_SEARCH_PREFIX + isbn);
+         return null;
+      }
+
+      jObject = new JSONObject(jsonString);
+      Book book = new Book();
+      String key = "ISBN:" + isbn;
+
+      JSONObject jsonBook = jObject.getJSONObject(key);
+
+      book.title = (jsonBook.getJSONObject("details").getString("title"));
+
+      if (jsonBook.getJSONObject("details").has("isbn_10")) {
+         book.isbn10 = (jsonBook.getJSONObject("details").getJSONArray("isbn_10").getString(0));
+      }
+
+      if (jsonBook.getJSONObject("details").has("isbn_13")) {
+         book.isbn13 = (jsonBook.getJSONObject("details").getJSONArray("isbn_13").getString(0));
+      }
+
+      if (jsonBook.getJSONObject("details").has("subtitle")) {
+         book.subTitle = (jsonBook.getJSONObject("details").getString("subtitle"));
+      }
+
+      if (jsonBook.getJSONObject("details").has("publish_date")) {
+         Date d = DateUtil.parse(jsonBook.getJSONObject("details").getString("publish_date"));
+         if (d != null) {
+            book.datePubStamp = (d.getTime());
+         }
+      }
+
+      if (jsonBook.getJSONObject("details").has("authors")) {
+         book.authors.add(new Author(jsonBook.getJSONObject("details").getJSONArray("authors").getJSONObject(0)
+                  .getString("name")));
+      }
+
+      if (jsonBook.getJSONObject("details").has("publishers")) {
+         book.publisher = (jsonBook.getJSONObject("details").getJSONArray("publishers").getString(0));
+      }
+
+      if (jsonBook.getJSONObject("details").has("subjects")) {
+         JSONArray resultArray = jsonBook.getJSONObject("details").getJSONArray("subjects");
+         String subjects = "";
+
+         for (int i = 0; i < resultArray.length(); i++) {
+            subjects += resultArray.getString(i);
+         }
+         book.subject = subjects;
+      }
+
+      if (jsonBook.getJSONObject("details").has("physical_format")) {
+         book.format = (jsonBook.getJSONObject("details").getString("physical_format"));
+      }
+
+      return book;
+   }
+
+   public ArrayList<Book> getBooks(String searchTerm, int startIndex) {
+      ArrayList<Book> books = new ArrayList<Book>();
+      if (startIndex < 1) {
+          // don't allow zero or neg, just set to 1
+          startIndex = 1;
+      }
+      String url = OpenLibraryDataSource.OL_BOOK_SEARCH_PREFIX+searchTerm+"&limit=10&offset="+startIndex;
+	
+      String response = httpHelper.performGet(url, null, null, null);
+	
+      if (application.debugEnabled) {
+    	  Log.d(Constants.LOG_TAG, "HTTP request to URL " + url);
+    	  Log.d(Constants.LOG_TAG, "HTTP response\n" + response);
+      }
+	
+      if ((response == null) || response.contains(HttpHelper.HTTP_RESPONSE_ERROR)) {
+    	  Log.w(Constants.LOG_TAG, "HTTP request returned no data (null) - " + url);
+    	  return books;
+      }
+	
+      try {
+    	  books = getBookDetails(response);
+      } catch (JSONException e) {
+    	  Log.e(Constants.LOG_TAG, e.getMessage(), e);
+      }
+	
+      return books;
+   }
+
+   private ArrayList<Book> getBookDetails(String jsonString) throws JSONException {
+      ArrayList<Book> books = new ArrayList<Book>();
+      jObject = new JSONObject(jsonString);
+      JSONArray resultArray = jObject.getJSONArray("docs");
+
+      for (int i = 0; i < resultArray.length(); i++) {
+         
+         JSONObject jsonBook = resultArray.getJSONObject(i);
+         Book book = new Book();
+         
+         if (jsonBook.has("title")) {
+        	 book.title = jsonBook.getString("title");
+         }
+         
+         if (jsonBook.has("isbn")) {
+        	 JSONArray jsonISBN = jsonBook.getJSONArray("isbn");
+             for (int k = 0; k < jsonISBN.length(); k++) {
+            	 if(jsonISBN.getString(k).length() == 10)
+            	 {
+            		 book.isbn10 = jsonISBN.getString(k);
+            	 }
+            	 else if(jsonISBN.getString(k).length() == 13)
+            	 {
+            		 book.isbn13 = jsonISBN.getString(k);
+            	 }
+             }
+         }
+
+         if (jsonBook.has("subtitle")) {
+            book.subTitle = (jsonBook.getString("subtitle"));
+         }
+
+         if (jsonBook.has("first_publish_year")) {
+            Date d = DateUtil.parse(jsonBook.getString("first_publish_year"));
+            if (d != null) {
+               book.datePubStamp = (d.getTime());
+            }
+         }
+
+         if (jsonBook.has("author_name")) {
+        	 JSONArray jsonAuthor = jsonBook.getJSONArray("author_name");
+             for (int j = 0; j < jsonAuthor.length(); j++) {
+            	 book.authors.add(new Author(jsonAuthor.getString(j)));
+             }
+         }
+
+         if (jsonBook.has("publisher")) {
+            book.publisher = (jsonBook.getJSONArray("publisher").getString(0));
+         }
+
+         if (jsonBook.has("subject")) {
+            JSONArray resultArray2 = jsonBook.getJSONArray("subject");
+            String subjects = "";
+
+            for (int j = 0; j < resultArray2.length(); j++) {
+               subjects += resultArray2.getString(j)+", ";
+            }
+            book.subject = subjects;
+         }
+
+         if (jsonBook.has("physical_format")) {
+            book.format = (jsonBook.getString("physical_format"));
+         }
+
+         books.add(book);
+      }
+
+      return books;
+   }
 }
